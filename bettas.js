4b2a0b4968eb8336cdc0ce76d1b123c50e3e409a
bettawave <soumajinth@gmail.com>
2026-05-25 10:19:45 +0530

    Update bettas.js

--- bettas.js
+++ bettas.js
@@ -1,35 +1,41 @@
 const fishes = [
 
   {
-    name:"Red splnden male",
+    category:"Males",
+    name:"Red Splenden Male",
     price:"₹600-1000",
     image:"https://i.ibb.co/0ptr1dr5/red-splenden.png"
   },
-   {
-    name:"Red splnden female",
-    price:"₹450",
-    image:"https://upload.wikimedia.org/wikipedia/commons/0/0e/Betta_splendens.jpg"
-  },
 
   {
+    category:"Males",
     name:"OHM Betta",
     price:"₹100-300",
     image:"https://upload.wikimedia.org/wikipedia/commons/7/76/Halfmoon_Betta.jpg"
   },
 
   {
+    category:"Males",
     name:"Crowntail Betta",
     price:"₹200-350",
     image:"https://upload.wikimedia.org/wikipedia/commons/5/5f/Crowntail_betta.jpg"
+  },
+
+  {
+    category:"Females",
+    name:"Red Splenden Female",
+    price:"₹450",
+    image:"https://upload.wikimedia.org/wikipedia/commons/0/0e/Betta_splendens.jpg"
   }
 
 ];
 
-let output = "";
+let maleOutput = "";
+let femaleOutput = "";
 
 fishes.forEach(fish => {
 
-  output += `
+  const card = `
 
     <div class="card">
 
@@ -37,12 +43,33 @@ fishes.forEach(fish => {
 
       <h2>${fish.name}</h2>
 
-      <p>${fish.price}</p>
+      <p class="price">${fish.price}</p>
 
     </div>
 
   `;
 
+  if(fish.category === "Females"){
+    femaleOutput += card;
+  }
+  else{
+    maleOutput += card;
+  }
+
 });
 
-document.getElementById("fish-container").innerHTML = output;
+document.getElementById("fish-container").innerHTML = `
+
+  <h1 class="section-title">Male Bettas</h1>
+
+  <div class="container">
+    ${maleOutput}
+  </div>
+
+  <h1 class="section-title">Female Bettas</h1>
+
+  <div class="container">
+    ${femaleOutput}
+  </div>
+
+`;

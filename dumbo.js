21ed9e98b3f4abd98d354e30648c6d5263bc1add
bettawave <soumajinth@gmail.com>
2026-05-26 09:21:41 +0530

    Create dumbo.js

---
+++ dumbo.js
@@ -0,0 +1,56 @@
+const dumbo = [
+
+  {
+    name:"Dumbo Ear OHM",
+
+    pair:"₹500",
+    male:"₹350",
+    female:"₹200",
+
+    images:[
+      "https://i.ibb.co/r205DHb9/DOHM.jpg",
+      "https://i.ibb.co/GQNJqVFZ/dohmw.jpg"
+    ]
+  }
+
+];
+
+let output = "";
+
+dumbo.forEach(fish => {
+
+  let imageHTML = "";
+
+  fish.images.forEach(img => {
+
+    imageHTML += `
+      <img src="${img}" class="fish-image">
+    `;
+
+  });
+
+  output += `
+
+    <div class="card">
+
+      <div class="image-grid">
+        ${imageHTML}
+      </div>
+
+      <h2>${fish.name}</h2>
+
+      <div class="price-box">
+
+        <p><b>Pair:</b> ${fish.pair}</p>
+        <p><b>Male:</b> ${fish.male}</p>
+        <p><b>Female:</b> ${fish.female}</p>
+
+      </div>
+
+    </div>
+
+  `;
+
+});
+
+document.getElementById("dumbo-container").innerHTML = output;

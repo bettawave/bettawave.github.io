403b5e98f4e4b8e90302fb791aa50e093ff0247c
bettawave <soumajinth@gmail.com>
2026-05-26 12:50:52 +0530

    Update guppies.js

--- guppies.js
+++ guppies.js
@@ -2,52 +2,62 @@ const guppies = [
 
   {
     name: "Platinum Red Super Dumbo Ear",
-    image: "https://i.ibb.co/7N0p0rj/guppy1.jpg"
+    image: "https://i.ibb.co/9Hg0CDWk/platdum1.jpg",
+    price: "₹200 / Pair"
   },
 
   {
     name: "Blue Topaz",
-    image: "https://i.ibb.co/wN8Y8KX/guppy2.jpg"
+    image: "https://i.ibb.co/vvkKk3Ms/bluetopaz.jpg",
+    price: "₹200 / Pair"
   },
 
   {
     name: "Red Topaz",
-    image: "https://i.ibb.co/k4V4xQw/guppy3.jpg"
+    image: "https://i.ibb.co/XZMrDyCB/red-topaz.jpg",
+    price: "₹140 / Pair"
   },
 
   {
     name: "Short Body Blonde Koi",
-    image: "https://i.ibb.co/7y3vY5M/guppy4.jpg"
+    image: "https://i.ibb.co/wZYn5Sk7/sbbk1.jpg",
+    price: "₹200 / Pair"
   },
 
   {
     name: "Albino Topaz White",
-    image: "https://i.ibb.co/x8J2b5r/guppy5.jpg"
+    image: "https://i.ibb.co/1G1Xg0Fs/atw1.jpg",
+    price: "₹180 / Pair"
   },
 
   {
     name: "Gold Guppies",
-    image: "https://i.ibb.co/WfY4r8v/guppy6.jpg"
+    image: "https://i.ibb.co/qYYYC1t2/goldgup1.jpg",
+    price: "₹180 / Pair"
   },
 
   {
     name: "HB Red Rose Cauli Dorsal",
-    image: "https://i.ibb.co/BnJ1M6d/guppy7.jpg"
+    image: "https://i.ibb.co/dstSrDT6/hbrc1.jpg",
+    price: "₹200 / Pair"
   },
 
   {
-    name: "White Tuxido",
-    image: "https://i.ibb.co/6mWjW7g/guppy8.jpg"
+    name: "White Tuxedo",
+    image: "https://i.ibb.co/4gRJZfmT/wt1.jpg",
+    price: "₹100 / Pair"
   },
 
   {
     name: "Blue Dragon",
-    image: "https://i.ibb.co/fF2w6j8/guppy9.jpg"
+    image: "https://i.ibb.co/PvRzPGmD/bd1.jpg",
+    price: "₹150 / Pair"
   },
 
   {
     name: "Chilli Mosaic",
-    image: "https://i.ibb.co/Q9n9M0r/guppy10.jpg"
+    image: "https://i.ibb.co/vCMcjNhn/cm1.jpg",
+    price: "₹150 / Pair"
   }
 
 ];
@@ -60,7 +70,12 @@ guppies.forEach(guppy => {
 
     <div class="card">
       <img src="${guppy.image}" alt="${guppy.name}">
-      <h2>${guppy.name}</h2>
+      
+      <div class="card-content">
+        <h2>${guppy.name}</h2>
+        <p class="price">${guppy.price}</p>
+      </div>
+      
     </div>
 
   `;

c721aadb3a9166ff33e29b703ffafce4d00f32b5
bettawave <soumajinth@gmail.com>
2026-05-26 11:00:44 +0530

    Update crowntail.js

--- crowntail.js
+++ crowntail.js
@@ -1,13 +1,13 @@
 const fishes = [
 
   {
-    name:"Crowntail Betta",
+    name: "Crowntail Betta",
 
-    pair:"₹400-700",
-    male:"₹250-500",
-    female:"₹150-250",
+    pair: "₹400-700",
+    male: "₹250-500",
+    female: "₹150-250",
 
-    images:[
+    images: [
       "https://i.ibb.co/ycpRX2Zd/ct1.jpg",
       "https://i.ibb.co/fYnFr3r2/ct2.jpg",
       "https://i.ibb.co/93TPkZVp/ct3.jpg",
@@ -27,13 +27,7 @@ fishes.forEach(fish => {
   fish.images.forEach(img => {
 
     imageHTML += `
-
-      <img
-        src="${img}"
-        class="fish-image"
-        onerror="this.style.display='none'"
-      >
-
+      <img src="${img}" class="fish-image">
     `;
 
   });

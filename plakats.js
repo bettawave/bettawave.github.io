b5eebdebdd230537eb41be8a75e3aea7db1a5748
bettawave <soumajinth@gmail.com>
2026-05-26 12:01:14 +0530

    Update plakats.js

--- plakats.js
+++ plakats.js
@@ -1,26 +1,15 @@
 const fishes = [
 
   {
-    name:"Black Samurai HMPK",
+    name:"Black and blue Samurai HMPK",
 
-    pair:"₹1500-2500",
-    male:"₹1000-1800",
-    female:"₹500-800",
-
-    images:[
-      "https://upload.wikimedia.org/wikipedia/commons/0/0e/Betta_splendens.jpg"
-    ]
-  },
-
-  {
-    name:"Blue Samurai HMPK",
-
-    pair:"₹1800-3000",
-    male:"₹1200-2200",
-    female:"₹600-900",
+    pair:"₹500-700",
+    male:"₹350-550",
+    female:"₹250",
 
     images:[
-      "https://upload.wikimedia.org/wikipedia/commons/0/0e/Betta_splendens.jpg"
+      "https://i.ibb.co/cSStdDvT/sam1.jpg",
+"https://i.ibb.co/SDBy56jM/sam2.jpg"
     ]
   },
 
@@ -32,55 +21,70 @@ const fishes = [
     female:"₹400-700",
 
     images:[
-      "https://upload.wikimedia.org/wikipedia/commons/0/0e/Betta_splendens.jpg"
+      "https://i.ibb.co/spR16phC/nc1.jpg",
+"https://i.ibb.co/xSW15F5J/nc2.jpg"
     ]
   },
 
   {
     name:"Tancho HMPK",
 
-    pair:"₹1000-1800",
-    male:"₹700-1400",
-    female:"₹350-600",
+    pair:"₹1500-2000",
+    male:"₹1200-1400",
+    female:"₹500-600",
 
     images:[
-      "https://upload.wikimedia.org/wikipedia/commons/0/0e/Betta_splendens.jpg"
+      "https://i.ibb.co/7Jb9X1qF/tan1.jpg",
+"https://i.ibb.co/tpkbYqt1/tan2.jpg"
     ]
   },
 
   {
-    name:"FCCP Red HMPK",
+    name:"Full GOld HMPK",
 
-    pair:"₹1500-2500",
-    male:"₹1000-1800",
-    female:"₹500-800",
+    pair:"₹1200",
+    male:"₹1000",
+    female:"₹500",
 
     images:[
-      "https://upload.wikimedia.org/wikipedia/commons/0/0e/Betta_splendens.jpg"
+      "https://i.ibb.co/LXj8d8pd/gold1.jpg",
+"https://i.ibb.co/VWG94W9m/gold2.jpg"
+    ]
+  },
+  {
+    name:"FCCP Red HMPK",
+
+    pair:"₹650-1600",
+    male:"₹500-1300",
+    female:"₹400-600",
+
+    images:[
+      "https://i.ibb.co/Ngxx23nb/fccp1.jpg"
     ]
   },
 
   {
     name:"FCCP Yellow HMPK",
 
-    pair:"₹1500-2500",
-    male:"₹1000-1800",
+    pair:"₹750-1700",
+    male:"₹600-1400",
     female:"₹500-800",
 
     images:[
-      "https://upload.wikimedia.org/wikipedia/commons/0/0e/Betta_splendens.jpg"
-    ]
+      "https://i.ibb.co/kVKLL0b2/yfccp1.jpg",
+"https://i.ibb.co/LzvSqKy6/yfccp2.jpg"
+      ]
   },
 
   {
     name:"Hellboy Red HMPK",
 
     pair:"₹1200-2200",
-    male:"₹800-1600",
+    male:"₹500-1300",
     female:"₹400-700",
 
     images:[
-      "https://upload.wikimedia.org/wikipedia/commons/0/0e/Betta_splendens.jpg"
+"https://i.ibb.co/3mxJCTvV/rhell1.jpg"
     ]
   },
 
@@ -88,11 +92,11 @@ const fishes = [
     name:"Hellboy Yellow HMPK",
 
     pair:"₹1200-2200",
-    male:"₹800-1600",
+    male:"₹500-1300",
     female:"₹400-700",
 
     images:[
-      "https://upload.wikimedia.org/wikipedia/commons/0/0e/Betta_splendens.jpg"
+"https://i.ibb.co/bjxFbNqC/yhell1.jpg" 
     ]
   },
 

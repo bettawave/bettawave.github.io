b3079809e100feda47463a9305b3df8af3fad049
bettawave <soumajinth@gmail.com>
2026-05-27 11:59:54 +0530

    Update ohmbettas.js

--- ohmbettas.js
+++ ohmbettas.js
@@ -9,6 +9,11 @@ const fishes = [
     male:"₹150-300",
     female:"₹80-150",
 
+    // Cart Prices
+    pairPrice:500,
+    malePrice:300,
+    femalePrice:150,
+
     images:[
       "https://i.ibb.co/60Qx8cpb/ohm1.jpg",
       "https://i.ibb.co/fGP7kn9Y/ohm2.jpg",
@@ -56,9 +61,21 @@ fishes.forEach(fish => {
 
         <p><b>Pair:</b> ${fish.pair}</p>
 
+        <button onclick="addToCart('${fish.name} Pair', ${fish.pairPrice})">
+          Add Pair to Cart
+        </button>
+
         <p><b>Male:</b> ${fish.male}</p>
 
+        <button onclick="addToCart('${fish.name} Male', ${fish.malePrice})">
+          Add Male to Cart
+        </button>
+
         <p><b>Female:</b> ${fish.female}</p>
+
+        <button onclick="addToCart('${fish.name} Female', ${fish.femalePrice})">
+          Add Female to Cart
+        </button>
 
       </div>
 
@@ -69,3 +86,20 @@ fishes.forEach(fish => {
 });
 
 document.getElementById("ohm-container").innerHTML = output;
+
+
+// CART FUNCTION
+
+function addToCart(name, price) {
+
+  let cart = JSON.parse(localStorage.getItem("cart")) || [];
+
+  cart.push({
+    name: name,
+    price: price
+  });
+
+  localStorage.setItem("cart", JSON.stringify(cart));
+
+  alert(name + " added to cart!");
+}

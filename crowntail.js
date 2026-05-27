95a0166db2abd7f2774c99bc94dc0daf2f4f2fa6
bettawave <soumajinth@gmail.com>
2026-05-27 11:56:19 +0530

    Update crowntail.js

--- crowntail.js
+++ crowntail.js
@@ -7,11 +7,16 @@ const fishes = [
     male: "₹250-500",
     female: "₹150-250",
 
+    // Cart Prices
+    pairPrice: 700,
+    malePrice: 500,
+    femalePrice: 250,
+
     images: [
       "https://i.ibb.co/ycpRX2Zd/ct1.jpg",
-"https://i.ibb.co/fYnFr3r2/ct2.jpg",
-"https://i.ibb.co/93TPkZVp/ct3.jpg",
-"https://i.ibb.co/39m6yp0q/ct5.jpg",
+      "https://i.ibb.co/fYnFr3r2/ct2.jpg",
+      "https://i.ibb.co/93TPkZVp/ct3.jpg",
+      "https://i.ibb.co/39m6yp0q/ct5.jpg",
     ]
   }
 
@@ -47,9 +52,21 @@ fishes.forEach(fish => {
 
         <p><b>Pair:</b> ${fish.pair}</p>
 
+        <button onclick="addToCart('${fish.name} Pair', ${fish.pairPrice})">
+          Add Pair to Cart
+        </button>
+
         <p><b>Male:</b> ${fish.male}</p>
 
+        <button onclick="addToCart('${fish.name} Male', ${fish.malePrice})">
+          Add Male to Cart
+        </button>
+
         <p><b>Female:</b> ${fish.female}</p>
+
+        <button onclick="addToCart('${fish.name} Female', ${fish.femalePrice})">
+          Add Female to Cart
+        </button>
 
       </div>
 
@@ -60,3 +77,20 @@ fishes.forEach(fish => {
 });
 
 document.getElementById("crowntail-container").innerHTML = output;
+
+
+// CART FUNCTION
+
+function addToCart(name, price) {
+
+  let cart = JSON.parse(localStorage.getItem("cart")) || [];
+
+  cart.push({
+    name: name,
+    price: price
+  });
+
+  localStorage.setItem("cart", JSON.stringify(cart));
+
+  alert(name + " added to cart!");
+}

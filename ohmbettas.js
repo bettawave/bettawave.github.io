990516741a574ca657fb927a27d2f7763df9fd98
bettawave <soumajinth@gmail.com>
2026-05-29 15:06:39 +0530

    Update ohmbettas.js

--- ohmbettas.js
+++ ohmbettas.js
@@ -1,30 +1,29 @@
-```javascript id="7v7xw3"
 const ohmbettas = [
 
   {
     name: "Regular OHM Male",
-    image: "https://i.ibb.co/F4qCP9Bj/dt1.jpg",
+    image: "https://i.ibb.co/60Qx8cpb/ohm1.jpg",
     price: "₹300 / piece",
     cartPrice: 300
   },
 
   {
     name: "Regular OHM Female",
-    image: "https://i.ibb.co/nqRSLg6V/dt2.jpg",
+    image: "https://i.ibb.co/fGP7kn9Y/ohm2.jpg",
     price: "₹150 / piece",
     cartPrice: 150
   },
 
   {
     name: "Candy OHM Male",
-    image: "https://i.ibb.co/F4qCP9Bj/dt1.jpg",
+    image: "https://i.ibb.co/BHp1WVJp/ohm3.jpg",
     price: "₹500 / piece",
     cartPrice: 500
   },
 
   {
     name: "Candy OHM Female",
-    image: "https://i.ibb.co/nqRSLg6V/dt2.jpg",
+    image: "https://i.ibb.co/jZDd8BcT/cohm.jpg",
     price: "₹250 / piece",
     cartPrice: 250
   }
@@ -33,13 +32,19 @@ const ohmbettas = [
 
 const container = document.getElementById("fishContainer");
 
+let output = "";
+
 ohmbettas.forEach(item => {
 
-  container.innerHTML += `
+  output += `
 
     <div class="card">
 
-      <img src="${item.image}" alt="${item.name}">
+      <img
+        src="${item.image}"
+        alt="${item.name}"
+        class="fish-image"
+      >
 
       <div class="card-content">
 
@@ -48,7 +53,9 @@ ohmbettas.forEach(item => {
         <p class="price">${item.price}</p>
 
         <button onclick="addToCart('${item.name}', ${item.cartPrice})">
+
           Add to Cart
+
         </button>
 
       </div>
@@ -59,20 +66,26 @@ ohmbettas.forEach(item => {
 
 });
 
+container.innerHTML = output;
 
-// CART FUNCTION
+
+// ADD TO CART
 
 function addToCart(name, price) {
 
-  let cart = JSON.parse(localStorage.getItem("cart")) || [];
+  let cart =
+  JSON.parse(localStorage.getItem("cart")) || [];
 
   cart.push({
     name: name,
     price: price
   });
 
-  localStorage.setItem("cart", JSON.stringify(cart));
+  localStorage.setItem(
+    "cart",
+    JSON.stringify(cart)
+  );
 
   alert(name + " added to cart!");
+
 }
-```
